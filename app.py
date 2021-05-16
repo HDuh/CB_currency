980abe2b74661e2fe35f6f75b265d02269eddd90
Nick_D <gespi@bk.ru>
2021-05-16 23:02:33 +0300

    trying vue js

--- app.py
+++ app.py
@@ -1,12 +1,54 @@
-from flask import Flask
+from flask import Flask, render_template
+from jsonify.convert import jsonify
 
 app = Flask(__name__)
 
+app.debug = True
+
+
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        variable_start_string='%%',  # Default is '{{', I'm changing this because Vue.js uses '{{' / '}}'
+        variable_end_string='%%',
+    ))
+
+
+app = CustomFlask(__name__)  # This replaces your existing "app = Flask(__name__)"
+
 
 @app.route('/')
-def hello_world():
-    return 'Нажми меня '
+@app.route('/home')
+def main_page():
+    return render_template('index.html')
 
+
+@app.route('/books', methods=['GET'])
+def all_books():
+    return jsonify({
+        'status': 'success',
+        'books': BOOKS
+    })
+
+
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
 
 if __name__ == '__main__':
+    print('Server is running on localhost!')
     app.run()
